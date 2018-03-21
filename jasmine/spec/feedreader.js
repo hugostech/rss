44ed76fa1ffaee3b86aca12099556625faf4bd38
hugo <hugo.wang@grabone.co.nz>
2018-03-21 15:04:38 +1300

    test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -22,29 +22,50 @@ $(function() {
         });
 
 
-        /* TODO:
+        /*
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          */
+        it('all elements\' urls are vaild', function () {
+            for (let feed of allFeeds){
+                expect(feed['url']).toBeDefined();
+                expect(feed['url'].length).not.toBe(0);
+            }
+        });
 
 
-        /* TODO:
+        /*
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
          */
+        it('all elements\' names are vaild', function () {
+            for (let feed of allFeeds){
+                expect(feed['name']).toBeDefined();
+                expect(feed['name'].length).not.toBe(0);
+            }
+        });
     });
 
-
-    /* TODO: 写一个叫做 "The menu" 的测试用例 */
-
+    describe('The Menu',function () {
         /* TODO:
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
-
-         /* TODO:
+        it('shauld have default display hidden',function () {
+            expect($('body').class()).toContain('menu-hidden');
+        });
+        /* TODO:
           * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
           * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
           * 再次点击的时候是否隐藏。
           */
+        it('should have button to control menu display status', function () {
+
+        });
+
+    });
+
+
+
+
 
     /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
 
